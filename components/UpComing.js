42b0108c6be8f28a7bce7dd337edba988caa694c
evondev <trananhtuan400@gmail.com>
2020-09-05 13:07:03 +0700

    Notice birthday only

--- components/UpComing.js
+++ components/UpComing.js
@@ -4,14 +4,16 @@ import Template from "./Template";
 const UpComingUI = () => {
   return (
     <>
+      {/* <p>Upcoming UIs: <span>a lot of UIs</span></p>
+    <p>Images I'm using from <span>Rachelizmarvel</span> on <span>Dribbble</span></p> */}
       <Template
         title="Notice"
         hideCode={true}
         source="upcoming"
         html={`
         <div class="upcoming">
-          <p>Upcoming UIs: <span>a lot of UIs</span></p>
-          <p>Images I'm using from <span>Rachelizmarvel</span> on <span>Dribbble</span></p>
+          <p>Today is <span>my birthday</span>, you can give me a gift by click heart icon on the right of bottom 🥰</p>
+
         </div>
         `}
         css={`
